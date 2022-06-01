9a032e859ea3cedd9a955d0537b5721c16c788e7
fede <fedegambassi96@gmail.com>
2022-06-01 18:21:01 +0200

    First commit

---
+++ blog/__init__.py
@@ -0,0 +1,17 @@
+"""Initialize Flask app."""
+from flask import Flask
+
+
+def create_app():
+    """Create Flask application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    with app.app_context():
+        # Import parts of our application
+        from .home import routes
+
+        # Register Blueprints
+        app.register_blueprint(routes.home_bp)
+
+        return app
